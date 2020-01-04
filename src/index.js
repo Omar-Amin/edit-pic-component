0ded44c9229a10c8925b0af7f37886cf4c5a4ee3
Omar Amin <omar_75@hotmail.dk>
2020-01-04 17:07:11 +0100

    Setting up project

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import IndexPage from './pages/IndexPage';
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(<IndexPage />, document.getElementById('root'));
+
---
+++ src/pages/IndexPage.js
@@ -0,0 +1,23 @@
+import React from "react"
+import './IndexPage.css';
+
+class IndexPage extends React.Component {
+    constructor() {
+        super()
+
+        this.state = {
+            isEditing: false
+        }
+    }
+
+    render() {
+
+        return (
+            <div>
+                hello
+            </div>
+        )
+    }
+}
+
+export default IndexPage
