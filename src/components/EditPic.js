674fe0cee9e4c383e9c2b81dbc0ffc9fbe12c181
Omar Amin <omar_75@hotmail.dk>
2020-01-05 18:59:47 +0100

    Can now add points to the picture (not saved, and it is hardcoded regarding to the size)

--- src/components/EditPic.js
+++ src/components/EditPic.js
@@ -6,16 +6,67 @@ class EditPic extends React.Component {
         super()
 
         this.state = {
+            points: [] // {x:0, y:0}
+        }
+
+        this.addPoint = this.addPoint.bind(this)
+        this.setWrapper = this.setWrapper.bind(this)
+        this.onImgLoad = this.onImgLoad.bind(this)
+
+    }
+
+    componentWillMount() {
+        document.addEventListener('mousedown', this.handleClickInsidePic);
+
+    }
+
+    componentWillUnmount() {
+        document.removeEventListener('mousedown', this.handleClickInsidePic);
+    }
+
+    setWrapper(node) {
+        this.wrapperRef = node
+    }
+
+    addPoint(e) {
+        if (this.wrapperRef && this.wrapperRef.contains(e.target)) {
+            const { points } = this.state
+            //right now it works in a fixed way, should be fixed
+            points.push({ x: e.nativeEvent.offsetX + 620 - 508 / 2, y: e.nativeEvent.offsetY, id: points.length })
+            this.setState({
+                points: points
+            })
+            console.log(this.state.dimensions)
         }
     }
 
+    onImgLoad({ target: img }) {
+        this.setState({
+            dimensions: {
+                height: img.offsetHeight,
+                width: img.offsetWidth
+            }
+        });
+    }
 
     render() {
+        const { points } = this.state
 
         return (
-            <div className="container">
+            <div className="container" ref="picContainer">
                 <button className="button-style" onClick={this.props.saveEdit}>Save</button>
-                <img className="image-style" src="https://cnet3.cbsistatic.com/img/sMJz61a5p3QEOwkK6aIT3R2skgw=/2019/08/15/5dda0e7e-8042-4317-9ee8-8256b04b5dda/samsung-galaxy-note-10-plus-13.jpg"></img>
+                <div onClick={this.addPoint}>
+                    <img
+                        ref={this.setWrapper}
+                        alt="Item to be edited"
+                        onLoad={this.onImgLoad}
+                        className="image-style"
+                        src="https://images.unsplash.com/photo-1527555197883-98e27ca0c1ea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"></img>
+                    {points.map((point) => (
+                        <div key={point.id} style={{ position: "fixed", top: point.y, left: point.x, backgroundColor: "#fff" }}>+</div>
+                    ))}
+                </div>
+
             </div>
         )
     }
