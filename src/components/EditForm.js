39999a7a13904930401a14d239a7faf1067bd5c3
Omar Amin <omar_75@hotmail.dk>
2020-01-07 22:46:35 +0100

    Added "check" and "close" buttons and animations, now we have to add the functionality.

--- src/components/EditForm.js
+++ src/components/EditForm.js
@@ -10,13 +10,24 @@ class EditForm extends React.Component {
         }
     }
 
+    // TODO: Add a "delete" or "save" for each edit for
+    // when saving, the form closes but can be opened again and changed/deleted
     render() {
 
         return (
-            <div className="edit-container">
-                <textarea maxLength="50" className="input-title-style" type="text" name="Title" placeholder="Title"></textarea>
-                <textarea maxLength="220" className="input-description-style" type="text" name="Description" placeholder="Description"></textarea>
+            <div className="editform-container">
+                <div className="check-item">
+                    <span className="check-span">&#10003;</span>
+                </div>
+                <div className="close-item">
+                    <span className="close-span">&#10005;</span>
+                </div>
+                <div className="edit-container">
+                    <textarea maxLength="50" className="input-title-style" type="text" name="Title" placeholder="Title"></textarea>
+                    <textarea maxLength="220" className="input-description-style" type="text" name="Description" placeholder="Description"></textarea>
+                </div>
             </div>
+
         )
     }
 }
