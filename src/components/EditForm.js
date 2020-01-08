cdf97dfadfff11c60f680b0e9e214de617e0aff5
Omar Amin <omar_75@hotmail.dk>
2020-01-08 10:46:26 +0100

    Added a pointer design and can now close and open the form, but doesn't save yet.

--- src/components/EditForm.js
+++ src/components/EditForm.js
@@ -1,33 +1,63 @@
 import React from "react"
 import './EditForms.css'
 
+const editformElement = (
+    <div>
+        <div className="edit-container">
+            <textarea maxLength="50" className="input-title-style" type="text" name="Title" placeholder="Title"></textarea>
+            <textarea maxLength="220" className="input-description-style" type="text" name="Description" placeholder="Description"></textarea>
+        </div>
+    </div>
+);
+
 class EditForm extends React.Component {
     constructor() {
         super()
 
         this.state = {
-
+            isOpen: true
         }
+
+        this.closeForm = this.closeForm.bind(this)
+        this.openForm = this.openForm.bind(this)
     }
 
-    // TODO: Add a "delete" or "save" for each edit for
-    // when saving, the form closes but can be opened again and changed/deleted
+    componentWillMount() {
+        const { editKey } = this.props
+        this.setState({
+            editKey: editKey
+        })
+    }
+
+    closeForm() {
+        this.setState({ isOpen: false })
+    }
+
+    openForm() {
+        this.setState({ isOpen: true })
+    }
+
+    // TODO: Add functionality to "save" and "delete"
+    // when pressing "close" it should autosave what's inside the component
     render() {
-
+        const { isOpen } = this.state
         return (
-            <div className="editform-container">
-                <div className="check-item">
+            <div className="editform-container" style={{ height: 0, width: 0 }}>
+                {isOpen ? <div className="circleout" onClick={this.closeForm}>
+                    <div className="circlein">
+                    </div>
+                </div> : <div className="circleoutc" onClick={this.openForm}>
+                        <div className="circleinc">
+                        </div>
+                    </div>}
+                {isOpen ? <div className="check-item">
                     <span className="check-span">&#10003;</span>
-                </div>
-                <div className="close-item">
+                </div> : null}
+                {isOpen ? <div className="close-item">
                     <span className="close-span">&#10005;</span>
-                </div>
-                <div className="edit-container">
-                    <textarea maxLength="50" className="input-title-style" type="text" name="Title" placeholder="Title"></textarea>
-                    <textarea maxLength="220" className="input-description-style" type="text" name="Description" placeholder="Description"></textarea>
-                </div>
+                </div> : null}
+                {isOpen ? (editformElement) : null}
             </div>
-
         )
     }
 }
--- src/components/EditPic.js
+++ src/components/EditPic.js
@@ -66,7 +66,7 @@ class EditPic extends React.Component {
                         src="https://cnet3.cbsistatic.com/img/sMJz61a5p3QEOwkK6aIT3R2skgw=/2019/08/15/5dda0e7e-8042-4317-9ee8-8256b04b5dda/samsung-galaxy-note-10-plus-13.jpg"></img>
                     {points.map((point) => (
                         <div key={point.id} style={{ position: "fixed", top: point.y, left: point.x }}>
-                            <EditForm />
+                            <EditForm editKey={point.id} />
                         </div>
                     ))}
                 </div>
